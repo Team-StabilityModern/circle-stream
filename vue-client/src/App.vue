81d95ebfbbdfc7e4dda80837d36de565f9e3705e
Yi-Jyun Pan <pan93412@gmail.com>
2021-03-07 20:09:02 +0800

    feat(client/App): allow to choose channel

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <section class="section">
-    <div class="field">
-      <div class="control">
-        <input type="text" class="input" v-model="channelName" />
+  <section class="section" style="max-width: 50vw; margin: 0 auto">
+    <div class="field" v-if="channelName === null">
+      <div class="label">頻道名稱</div>
+      <div class="field is-grouped">
+        <div class="control">
+          <input type="text" class="input" v-model="newChannelName" />
+        </div>
+        <div class="control">
+          <button class="button is-primary" @click="applyChannelName()">
+            選擇
+          </button>
+        </div>
       </div>
     </div>
-    <MessageBox :sender="this.sender" ip="127.0.0.1" :channel="channelName" />
+    <MessageBox
+      v-if="channelName !== null"
+      :sender="this.sender"
+      ip="127.0.0.1"
+      :channel="channelName"
+    />
   </section>
 </template>
 
@@ -21,6 +34,13 @@ import "bulma";
 })
 export default class App extends Vue {
   sender: string = `Client${Math.round(Math.random() * 1000)}`;
+  newChannelName: string = "default";
+  channelName: string | null = null;
+
+  applyChannelName() {
+    const prompt = confirm(`是否連線到 ${this.newChannelName} 頻道？`);
+    if (prompt) this.channelName = this.newChannelName;
+  }
 }
 </script>
 
