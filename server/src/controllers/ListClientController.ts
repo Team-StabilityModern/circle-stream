c1410498cec7dbadccf373aa3aabcc03a4fee0f0
Yi-Jyun Pan <pan93412@gmail.com>
2021-03-08 18:55:59 +0800

    feat(server/controllers/listclient): enable this

--- server/src/controllers/ListClientController.ts
+++ server/src/controllers/ListClientController.ts
@@ -1,16 +1,32 @@
-import { joined } from "../services/main/joined";
-import { leftWrapper } from "../services/main/left";
-import { onMessageWrapper } from "../services/main/onMessage";
-import { IsIParam } from "../types/IParam";
+import Connections from "../Connections";
 import { IController } from "./IController";
+
+interface IParam {
+  channel: string;
+}
+
+function IsIParam(param: unknown): param is IParam {
+  const p = param as IParam;
+
+  return (
+    typeof p === 'object' &&
+    typeof p.channel === 'string'
+  );
+}
 
 /**
  * A endpoint to list all online clients: `/list`
  * @param fastify The fastify instance
  */
 const ListClientController: IController = (fastify) => {
-  fastify.get("/list", async function ListClient(request, reply) {
-    return {};
+  fastify.get("/list/:channel", async function ListClient(request, reply) {
+    const param = request.params;
+    if (IsIParam(param)) {
+      reply.code(200);
+      return Connections.getInstance(param.channel).listAllId();
+    }
+    reply.code(400);
+    return [];
   });
 }
 
--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,10 +1,12 @@
+import ListClientController from "./ListClientController";
 import MainController from "./MainController";
 
 /**
  * The controllers list to be used.
  */
 const controllers = [
-  MainController
+  MainController,
+  ListClientController,
 ]
 
 export default controllers;
